b4295f7304d07e8c37fc2419b79c71553ac13a3f
rushikeshkulkarni94 <rushirvk94@gmail.com>
2018-04-18 01:31:42 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,14 @@
+$(".smallgroup").click(function() {
+    $(".bigtext").toggle(); 
+    // alert("click");
+});
+
+$(".smallgroup2").click(function() {
+    $(".bigtext2").toggle(); 
+    // alert("click");
+});
+
+$(".smallgroup3").click(function() {
+    $(".bigtext3").toggle(); 
+    // alert("click");
+});
